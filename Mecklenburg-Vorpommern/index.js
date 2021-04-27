06098b76a03fd36ea54d267f3ac1b5825156fabe
sifatnabil <sifatnabil@gmail.com>
2021-04-27 17:55:14 +0600

    Completed scraping for Mecklenburg-Vorpommern region

--- Mecklenburg-Vorpommern/index.js
+++ Mecklenburg-Vorpommern/index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 const url = "https://www.kvmv.de/ases-kvmv/ases.jsf";
 const riStr = "Rheumatologie";
+const gaStr = "gastroenterologie";
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -18,7 +19,7 @@ const riStr = "Rheumatologie";
 
   await page.waitForSelector(searchSel);
 
-  await page.type(searchSel, riStr);
+  await page.type(searchSel, gaStr);
 
   const nameLinkSel = ".ases-arzt-name-fachgebiet-text a";
   await page.waitForSelector(nameLinkSel);
@@ -26,18 +27,44 @@ const riStr = "Rheumatologie";
   const nextPageBtn =
     "#arztlisteDataList_paginator_bottom > span.ui-paginator-next.ui-state-default.ui-corner-all";
 
+  const resultsAr = [];
+
   while (true) {
-    await page.waitForTimeout(10000);
+    await page.waitForTimeout(40000);
     const fetchHcpInfo = await page.evaluate(async () => {
       const cards = document.querySelectorAll(
         ".ases-arzt-name-fachgebiet-text a"
       );
-
       const details = [];
 
       cards.forEach(async (card, ind) => {
         card.click();
         await new Promise((resolve) => setTimeout(resolve, 30000));
+        const workplaces = document.querySelectorAll(
+          "div.ui-tabs-navscroller > ul > li > a"
+        );
+
+        workplaces.forEach((workplace) => {
+          workplace.click();
+        });
+
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+
+        const subSections = document.querySelectorAll(
+          `#arztlisteDataList\\:${ind}\\:detailsTabView > div.ui-tabs-navscroller > ul > li`
+        );
+
+        const subSectionsAr = [];
+        for (let j = 0; j < subSections.length; j++) {
+          const moreInfo = document.querySelector(
+            `#arztlisteDataList\\:${ind}\\:detailsTabView\\:${j}\\:result-praxis > div:nth-child(1)`
+          ).outerText;
+          const additionalMoreInfo = document.querySelector(
+            `#arztlisteDataList\\:${ind}\\:detailsTabView\\:${j}\\:result-praxis > div.ases-leistungsort-taetigkeit > ul`
+          ).outerText;
+          subSectionsAr.push(moreInfo + "\n" + additionalMoreInfo);
+        }
+
         const info = document.querySelector(
           `#arztlisteDataList\\:${ind}\\:detailsPanel > div`
         );
@@ -70,12 +97,17 @@ const riStr = "Rheumatologie";
           "Foreign Languages": foreignLanguages,
           "Additional Designation": additionalDesignation,
           "Additional Contracts": additionalContracts,
+          "Additional Info": subSectionsAr,
         });
       });
-      await new Promise((resolve) => setTimeout(resolve, 30000));
+      await new Promise((resolve) => setTimeout(resolve, 50000));
       return details;
     });
-    console.log(fetchHcpInfo);
+    const results = fetchHcpInfo;
+
+    for (const result of results) {
+      resultsAr.push(result);
+    }
 
     const getTabIndex = await page.evaluate((sel) => {
       const tabIndex = document.querySelector(sel).tabIndex;
@@ -89,4 +121,12 @@ const riStr = "Rheumatologie";
       break;
     }
   }
+
+  fs.writeFile("result_ga.json", "[", () => {});
+  for (const result of resultsAr) {
+    fs.appendFile("result_ga.json", JSON.stringify(result) + ",", () => {});
+  }
+  fs.appendFile("result_ga.json", "]", () => {});
+
+  await browser.close();
 })();
